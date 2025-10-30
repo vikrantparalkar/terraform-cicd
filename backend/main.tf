862d2f9f3a4a9d986852ea8e349fafb42695cba4
vikrantparalkar <vikrantparalkar21@gmail.com>
2025-10-31 02:25:48 +0530

    endd

---
+++ backend/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create S3 bucket for Terraform state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-s3-vikrant5757"
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
+# Enable versioning (helps with state recovery)
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# DynamoDB for state locking
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Dev"
+  }
+}
