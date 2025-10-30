6895bd0df8b167b7aff4687c30f25df5321e75fb
vikrantparalkar <vikrantparalkar21@gmail.com>
2025-10-30 22:45:22 +0530

    4th

--- backend.tf
+++ backend.tf
@@ -3,8 +3,10 @@ terraform {
     bucket = "my-s3-vikrant5757"
     key    = "state"
     region = "us-east-1"
-    use_lockfile = "mera-table"
+    #use_lockfile = "mera-table"
     encrypt        = true
+    #kms_key_id     = "THE_ID_OF_THE_KMS_KEY"
+    dynamodb_table = "mera-table"
     
   }
 }
