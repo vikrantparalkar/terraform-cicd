d7ff15e2437a7c1d045205f75b0455d7354abe4a
vikrantparalkar <vikrantparalkar21@gmail.com>
2025-10-31 02:18:35 +0530

    final

--- backend.tf
+++ backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "my-s3-vikrant5757"
-    key    = "state"
+    key    = "terraform/state"
     region = "us-east-1"
     use_lockfile = true
-    #dynamodb_table = "mera-table"
-    encrypt        = true 
+    encrypt      = true 
   }
 }
